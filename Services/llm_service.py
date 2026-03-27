f6eb2f03a6d6f48f744d1442d24205960e1d5952
Gudboy <ninadnimkar2021@gmail.com>
2026-03-28 00:11:33 +0530

    changed the llm model to gpt-4o-mini

--- Services/llm_service.py
+++ Services/llm_service.py
@@ -1,32 +1,32 @@
 from sarvamai import SarvamAI
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
 from Services.prompt_builder import build_prompt
 load_dotenv()
 
-client = SarvamAI(api_subscription_key=os.getenv("SARVAM_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def summarize(text: str) -> str:
-    response = client.chat.completions(
-        messages=[{
-            "role": "user",
-            "content": f'''
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+            messages= [{"role": "user", 
+                        "content": f'''
             extract the core concepts from this content.
             remove formatting, repetition and irrelevant text
-            {text}'''
-        }]
-    )
+            {text}'''}],
+            max_tokens=500,
+        )
 
     return response.choices[0].message.content
 
 def explain(summary: str, style: str, langauge: str) -> str:
     prompt = build_prompt(summary, style, langauge)
 
-    response = client.chat.completions(
-        messages=[{
-            "role": "user",
-            "content": prompt
-        }]
-    )
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+            messages= [{"role": "user", "content": prompt}],
+            max_tokens=500,
+        )
 
     return response.choices[0].message.content
