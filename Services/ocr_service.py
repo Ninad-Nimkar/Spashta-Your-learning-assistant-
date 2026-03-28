70bfe2147a4f388d1ebce9b98460ffbfbc49bea1
Gudboy <ninadnimkar2021@gmail.com>
2026-03-28 23:16:15 +0530

    added vision

--- Services/ocr_service.py
+++ Services/ocr_service.py
@@ -1,13 +1,12 @@
 import pdfplumber
-from PIL import Image
 import io
+import os
 from openai import OpenAI
-from fastapi import FastAPI
 import base64
-from llm_service import client
-from prompt_builder import build_prompt
 from dotenv import load_dotenv
 load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def extract_text(file_bytes: bytes, filename) -> str:
     extracted_text = ""
@@ -17,29 +16,23 @@ def extract_text(file_bytes: bytes, filename) -> str:
         with pdfplumber.open(io.BytesIO(file_bytes)) as pdf:
             for page in pdf.pages:
                 extracted_text += page.extract_text() or ""
+        return extracted_text
 
     #Image handeling
     else:
         base64_image = base64.b64decode(file_bytes).decode("utf-8")
 
         response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[{
-                "role": "user",
-                "content": [{
-                    "type": "text",
-                    "text":f"extract text"
-                },
-                {
-                    "type": "image_url",
-                    "image_url": {"url": f"data:image/png;base64,{base64_image}"}
-                }]
-            }],
-            max_tokens=1500,
-            temperature=0.3
+            model="gpt-4o-mini"
+            messages=[{"role": "user", 
+                       "content": [{"type": "text", "text": "Extracty all the text fro this page. Return Only the text."},
+                                    "type": "image_url",
+                                    "image_url": {"url": f"data:image/png;base64,{base64_image}
+                                                },
+                                            ],
+                                        }
+                                    ],
+                                    max_tokens=1500
         )
-
-    return{
-        "input_type": "image",
-        "solution": response.choices[0].message.content
-    }
+        extracted_text = response.choices[0].message.content
+        return extracted_text
